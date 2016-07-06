cb14f255dd9c12489da98158c981e89fac8deeb1
Craig Bennett <ctbennettii@gmail.com>
2016-07-06 04:49:31 -0400

    .
    .

--- Shutdown Wait/Form1.Designer.vb
+++ Shutdown Wait/Form1.Designer.vb
@@ -56,6 +56,8 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.ForeColor = System.Drawing.SystemColors.Highlight
         Me.Label1.Location = New System.Drawing.Point(8, 44)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(178, 13)
@@ -119,6 +121,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ClientSize = New System.Drawing.Size(487, 161)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.TextBox1)
